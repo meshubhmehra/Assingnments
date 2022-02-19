3cc6e0a9284db9d474feb0e4d9c039a820bb63da
shubham mehra <dev.shubhamehra@gmail.com>
2022-02-19 11:43:59 +0530

    added

---
+++ que1.js
@@ -0,0 +1,15 @@
+/* Create a js file which contains variable declaration for a form which asks following basic details :
+ (What is your Name, Email, age, address, phone number, gender,
+ Are you married)*/
+
+let name = "";
+let email = "";
+let age = "";
+let address = "";
+let phone_no = "";
+let gender = "";
+let is_married = "";
+
+console.log(
+  `i am ${name} my email is ${email} i am ${age} year old,belonged to ${address},${phone_no} this is my no and i am ${is_married}.`
+);
