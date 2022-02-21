d2d4e0dfd2e46b76f8e30598f8e901cd618cc0d1
meshubhmehra <99950616+meshubhmehra@users.noreply.github.com>
2022-02-21 14:59:23 +0530

    Add files via upload

---
+++ assingn6.js
@@ -0,0 +1,71 @@
+// 1. Find sum of first 10 natural numbers
+
+// let num = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//   num += i;
+// }
+// console.log(num);
+
+// i = 1;
+// while (i <= 10) {
+//   num += i;
+//   i += 1;
+// }
+// console.log(num);
+
+// i = 1;
+// do {
+//   num += i;
+//   i += 1;
+// } while (i <= 10);
+// console.log(num);
+
+// 2. Print fibbonaci series upto first 10 numbers.
+
+// let x = 0;
+// let y = 1;
+// let z;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(x);
+//   n = x + y;
+//   x = y;
+//   y = n;
+// }
+
+// let i = 1;
+// while (i <= 10) {
+//   let z = x + y;
+//   console.log(x);
+//   n = x + y;
+//   x = y;
+//   y = z;
+//   i++;
+// }
+
+// let i = 1;
+// do {
+//   let z = x + y;
+//   console.log(x);
+//   n = x + y;
+//   x = y;
+//   y = z;
+//   i++;
+// } while (i <= 10);
+
+// 3. Print all the keys and values of employee object used in first problem
+
+// const employee = {
+//   name: "abc",
+//   email: "abc@gmail.com",
+//   age: 25,
+//   address: "indore",
+//   phone_no: "1234567890",
+//   gender: "male",
+//   is_married: "no",
+// };
+
+// for (let key in employee) {
+//   console.log(key + ":" + employee[key]);
+// }
