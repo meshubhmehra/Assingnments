d8474e5d6d1c914cc37152fa82ba5f986bfe0532
meshubhmehra <99950616+meshubhmehra@users.noreply.github.com>
2022-02-21 11:36:50 +0530

    Add files via upload

---
+++ assingn5.js
@@ -0,0 +1,47 @@
+// 1. Take 2 numbers and print the smallest one amongst them
+
+// let a = 72;
+// let b = 92;
+
+// if (a > 5) {
+//   console.log(a + " " + "Is bigger then" + " " + b);
+// } else {
+//   console.log(b + " " + "Is bigger then" + " " + a);
+// }
+
+// 2. Take 3 numbers and print the biggest one amongst them
+
+// let c = 82;
+
+// if (a < b) {
+//   if (b < c) {
+//     console.log(c + " " + "Is bigger then" + a + "," + b);
+//   } else {
+//     console.log(b + " " + "Is bigger then" + a + "," + c);
+//   }
+// } else {
+//   console.log(a + " " + "Is bigger then" + b + "," + c);
+// }
+
+// 3. Use switch case to return a grade for the score, either "A", "B", "C", "D", or "F". (Take convension for numbers and grades by yourself)
+// Anything below 50 is F
+
+// const grade = 95;
+
+// switch (true) {
+//   case grade >= 95:
+//     console.log("A");
+//     break;
+//   case grade >= 85:
+//     console.log("B");
+//     break;
+//   case grade >= 60:
+//     console.log("C");
+//     break;
+//   case grade >= 50:
+//     console.log("D");
+//     break;
+
+//   default:
+//     console.log("F");
+// }
