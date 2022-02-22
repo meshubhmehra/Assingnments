8afaabe90ca582695556cd6723800e30d95c57b3
meshubhmehra <99950616+meshubhmehra@users.noreply.github.com>
2022-02-22 12:07:05 +0530

    Add files via upload

---
+++ assingn8.js
@@ -0,0 +1,33 @@
+// 1. Create a function that takes a number and prints all even numbers upto that number
+
+// const even_no = (n) => {
+//   for (i = 0; i <= n; i += 2) {
+//     console.log(i);
+//   }
+// };
+// even_no(10);
+
+// let emp = [
+//   (emp_1 = {
+//     name: "abc",
+//     age: 25,
+//   }),
+//   (emp_2 = {
+//     name: "name",
+//     age: 50,
+//   }),
+//   (emp_3 = {
+//     name: "demo",
+//     age: 41,
+//   }),
+//   (emp_4 = {
+//     name: "ham",
+//     age: 12,
+//   }),
+// ];
+
+// const new_emp = (emp, age) => {
+//   return emp.filter((emp) => emp.age < age);
+// };
+
+// console.log(new_emp(emp, 20));
