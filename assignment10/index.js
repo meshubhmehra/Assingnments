f7467cbc38099d0c61f66b56c7a0bd898d285ccf
meshubhmehra <99950616+meshubhmehra@users.noreply.github.com>
2022-02-28 10:21:17 +0530

    Add files via upload

--- assignment10/index.js
+++ assignment10/index.js
@@ -1,1 +1,42 @@
+const Employees = require("./emp");
 
+const employeeArray = [
+  {
+    id: 1,
+    name: "vinod",
+    age: 20,
+    salary: 51000,
+  },
+  {
+    id: 2,
+    name: "naman",
+    age: 26,
+    salary: 720000,
+  },
+  {
+    id: 3,
+    name: "shreya",
+    age: 25,
+    salary: 520000,
+  },
+  {
+    id: 4,
+    name: "vgf",
+    age: 53,
+    salary: 720000,
+  },
+];
+
+var obj = new Employees(employeeArray);
+
+let sortedArray = obj.sortEmployees("name", "<");
+console.log(sortedArray);
+
+sortedArray = obj.sortEmployees("name", ">");
+console.log(sortedArray);
+
+let filteredArray = obj.filterByAge("=", 20);
+console.log(filteredArray);
+
+filteredArray = obj.filterByAge("<", 50);
+console.log(filteredArray);
