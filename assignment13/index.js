7df67a5c72f74d7c88a04106db8db3a8efe79082
meshubhmehra <99950616+meshubhmehra@users.noreply.github.com>
2022-03-07 14:53:59 +0530

    Add files via upload

--- assignment13/index.js
+++ assignment13/index.js
@@ -1,1 +1,35 @@
+const emp_data = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const emp = [
+        {
+          name: "shubham",
+          city: "bhopal",
+          age: 25,
+        },
+        {
+          name: "shreya",
+          city: "indore",
+          age: 22,
+        },
+        {
+          name: "Rahul",
+          city: "pune",
+          age: 27,
+        },
+        {
+          name: "shreya",
+          city: "indore",
+          age: 22,
+        },
+      ];
+      resolve(emp);
+    }, 2000);
+  });
+};
 
+async function getData() {
+  const data = await emp_data();
+  console.log(data);
+}
+getData();
